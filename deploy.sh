c17e0c2dc9364b7fd058d7412f2c22a1e44d23b4
dobrealeksa <dobre.aleksa@icloud.com>
2026-09-24 22:16:41 +0200

    new deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if docker pull ghcr.io/dobrealeksa/hello:v1 | grep "Downloaded newer image"; then
+    docker stop pipeline-main
+    docker rm pipeline-main
+    docker run -d -p 8000:8000 --name pipeline-main ghcr.io/dobrealeksa/hello:v1 
+else
+    if docker ps | grep -i pipeline-main; then
+        exit 0
+    else
+        docker start pipeline-main
+    fi
+    echo "Nema nove verzije"
+    exit 0
+fi
+
