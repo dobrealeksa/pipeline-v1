9273f458cae03a0b9196996b98e8a0e7391bd17a
dobrealeksa <dobre.aleksa@icloud.com>
2026-09-24 20:23:34 +0200

    CI v1.3

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if curl -s http://localhost:8000 | grep -q "Hello"; then
+if curl -s --retry 10 --retry-delay 1 --retry-all-errors http://localhost:8000 | grep -q "Hello"; then
   echo "Test prosao!"
   exit 0
 else
