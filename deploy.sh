fb1faa5a45999f2ab337f1b37bc670ff3780b8b2
dobrealeksa <dobre.aleksa@icloud.com>
2026-09-24 22:16:41 +0200

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if docker pull ghcr.io/dobrealeksa/hello:v1 | grep "Downloaded newer image"; then
-    docker stop pipeline-main
-    docker rm pipeline-main
+    docker stop pipeline-main 2>/dev/null || true
+    docker rm pipeline-main || true
     docker run -d -p 8000:8000 --name pipeline-main ghcr.io/dobrealeksa/hello:v1 
 else
-    if docker ps | grep -i pipeline-main; then
+    if docker ps | grep -q pipeline-main; then
         exit 0
     else
         docker start pipeline-main
